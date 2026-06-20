8703baf71f87f8a9bababd34c0ac1a93ed8fb06a
Mahlomu <lehlohonolongoepe713@gmail.com>
2026-06-20 21:58:23 +0200

    Week 2 Express API completed

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("My Week 2 API!");
+});
+
+app.post("/user", (req, res) => {
+  const { name, email } = req.body;
+
+  if (!name || !email) {
+    return res.status(400).json({
+      error: "Name and email are required",
+    });
+  }
+
+  res.json({
+    message: `Hello, ${name}!`,
+  });
+});
+
+app.get("/user/:id", (req, res) => {
+  const id = req.params.id;
+
+  res.send(`User ${id} profile`);
+});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
